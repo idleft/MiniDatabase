ad7cb94a0094bc9c103a29ac4cdd3ed764c440c7
Joyce <joyceshin514@gmail.com>
2014-11-16 22:23:10 -0800

    test script changed

--- test.sh
+++ test.sh
@@ -2,7 +2,21 @@
 unzip $1.zip
 cd $1
 cd codebase
-cd rbf
+cd ix
 make clean
 make
-./rbftest
+./ixtest1
+./ixtest2
+./ixtest3
+./ixtest4a
+./ixtest4b
+./ixtest4c
+./ixtest5
+./ixtest6
+./ixtest7
+./ixtest8
+./ixtest_extra_1
+./ixtest_extra_2a
+./ixtest_extra_2b
+./ixtest_extra_2c
+./ixtest_extra_2d
